e6c6b13e9e9ff8ff9e17b11521653341d676ca46
Disraely <Disraely@192.168.1.107>
2021-07-29 16:48:03 -0400

    Correccion de problemas

--- ProyectoFinal/src/visual/Empleado.java
+++ ProyectoFinal/src/visual/Empleado.java
@@ -68,23 +68,6 @@ public class Empleado extends JDialog {
 	private JPanel panel_7;
 	private JFormattedTextField formattedTextField;
 
-	/**
-	 * Launch the application.
-	 */
-	//hola
-	/*public static void main(String[] args) {
-		try {
-			Empleado dialog = new Empleado();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}*/
-
-	/**
-	 * Create the dialog.
-	 */
 	public Empleado() {
 		setBounds(100, 100, 684, 491);
 		setLocationRelativeTo(null);
